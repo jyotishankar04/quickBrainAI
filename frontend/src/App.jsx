c9f0dd114af7981310874e1e62d045515d85a726
sarojkumar <sarojojha2457@gmail.com>
2025-03-06 13:08:30 +0530

    Added a paragraph to the div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
       <div className="bg-slate-300 w-1/2 h-1/2 flex flex-col justify-center items-center">
         <h1 className="text-4xl text-blue-600">Quick Brain AI</h1>
         <p className="text-xl text-gray-900">An AI powered note taking app</p>
+        <p className="text-xl text-gray-900">You can take questions from pdf from this</p>
       </div>
     </div>
   )
